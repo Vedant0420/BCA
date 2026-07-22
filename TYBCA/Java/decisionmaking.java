cd8f499e52da8dee0183de2b96fb0d0dee2d5ca3
vedant <vedantkhedkar007@gmail.com>
2026-07-22 15:07:33 +0530

    Python Lab assignment

--- TYBCA/Java/decisionmaking.java
+++ TYBCA/Java/decisionmaking.java
@@ -54,20 +54,25 @@
 
 // Write a program which accepts two numbers from user and perform arthmetic operations on them
 
+// import java.util.Scanner;
+// class operations
+// {
+//     public static void main(String args[])
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter first number: ");
+//         int num1 = sc.nextInt();
+//         System.out.print("Enter second number: ");
+//         int num2 = sc.nextInt();
+//         System.out.println("Addition: " + (num1 + num2));
+//         System.out.println("Subtraction: " + (num1 - num2));
+//         System.out.println("Multiplication: " + (num1 * num2));
+//         System.out.println("Division: " + (num1 / num2));
+//         sc.close();
+//     }
+// }
+
+
+// Check Largest number by taking three outputs
 import java.util.Scanner;
-class operations
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-        System.out.println("Addition: " + (num1 + num2));
-        System.out.println("Subtraction: " + (num1 - num2));
-        System.out.println("Multiplication: " + (num1 * num2));
-        System.out.println("Division: " + (num1 / num2));
-        sc.close();
-    }
-}
+class 
